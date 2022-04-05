1527db90b4359c2ae3438ba6c67709c9f12bc806
Megan <mleavens19@gmail.com>
2022-04-05 18:22:33 -0400

    first commit

--- src/components/context/TaskContext.js
+++ src/components/context/TaskContext.js
@@ -9,6 +9,15 @@ export const TaskProvider = ({children}) => {
     const [taskEdit, setTaskEdit] = useState({   task: {}, 
         edit: false});
 
+    //to fetch the task data
+    const fetchTask = async () => {
+        const response = await fetch(`http://localhost:5000/tasks?_sort=id&order=desc`);
+
+        const data = response.json();
+        setTaskList(data);
+    }
+    
+
     //to delete the task
     const deleteTask = (id) => {
         setTaskList(taskList.filter((task) => task.id !== id));
